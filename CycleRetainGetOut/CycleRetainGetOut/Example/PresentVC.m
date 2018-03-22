5d027d69fb2da28af2b3a9a9e2ec8c8d92e81771
XTShow <447800853@qq.com>
2018-03-23 07:59:53 +0800

    继续验证情况

--- CycleRetainGetOut/CycleRetainGetOut/Example/PresentVC.m
+++ CycleRetainGetOut/CycleRetainGetOut/Example/PresentVC.m
@@ -20,28 +20,38 @@
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor grayColor];
     
-    [self useTimerInWeak];
+//    [self useTimerInWeak];
 //    [self checkTimerRelease];
+//    [self checkTimerReleaseInTradition];
+}
+
+-(void)viewDidDisappear:(BOOL)animated{
+    [super viewDidDisappear:animated];
+    //checkTimerReleaseInTradition
+    for (NSTimer *timer in self.timerArray) {
+        [timer invalidate];
+    }
 }
 
 -(void)dealloc{
     
     NSLog(@"%@%s",[self class],__func__);
     //常规使用
-    NSLog(@"before-%@",self.timer);
-    [self.timer invalidate];
-    NSLog(@"after-%@",self.timer);
+//    NSLog(@"before-%@",self.timer);
+//    [self.timer invalidate];
+//    NSLog(@"after-%@",self.timer);
     
     //检测NSTimer的释放（checkTimerRelease）
-//    for (NSTimer *timer in self.timerArray) {
-//        [timer invalidate];
-//    }
+    for (NSTimer *timer in self.timerArray) {
+        [timer invalidate];
+    }
 }
 
 -(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
     [self dismissViewControllerAnimated:YES completion:nil];
 }
 
+//模拟常规使用NSTimer
 - (void)useTimerInWeak {
     
     __weak __typeof__(self)weakSelf = self;
@@ -52,6 +62,7 @@
 
 }
 
+//检测NSTimer是否真的被释放了
 - (void)checkTimerRelease {
     
     self.timerArray = [NSMutableArray array];
@@ -62,6 +73,19 @@
         NSTimer *timer = [NSTimer XT_scheduledTimerWithTimeInterval:1 block:^{
             [weakSelf dosth];
         } userInfo:@"asd" repeats:YES];
+        
+        [self.timerArray addObject:timer];
+    }
+}
+
+//检测传统方式下，NSTimer是否真的被释放了
+- (void)checkTimerReleaseInTradition {
+    
+    self.timerArray = [NSMutableArray array];
+    
+    for (int i = 0; i < 10000; i++) {
+        
+        NSTimer *timer = [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(dosth) userInfo:nil repeats:YES];//即使此时repeats设置为NO，也不会释放内存
         
         [self.timerArray addObject:timer];
     }
